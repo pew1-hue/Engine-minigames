0cc5445660c8e9e771d32cf3e5d5b231fe533101
pew1-hue <pblabitoria@gmail.com>
2024-02-06 12:19:06 +0900

    0.0.0

--- engines/powerBall5.js
+++ engines/powerBall5.js
@@ -52,7 +52,6 @@ const powerBall5 = {
     showGame: () => {
         timerCountDown = interval * 60
         console.log("NEW GAME: ", game)
-        // console.log("Timer countdown:" + timerCountDown + "seconds")
     },
     getResult: async () => {
         try {
@@ -92,80 +91,14 @@ const powerBall5 = {
         }
         return sec
     },
-    // getResult: () => {
-    //     miniGames[0].resultTime = `Time: ${tempTime}:${tempSec}`
-    // },
     delay: (duration) => {
         return new Promise(resolve => setTimeout(resolve, duration))
     },
     showResult: async() => {
-        // const now = moment()
-        // game.result = varResult
-        // game.resultDateTime = now.toISOString().replace('T', '').substring(0, 19)
-        // previousGames.push(game)
-        // console.log(game)
         console.log("RESULTS: ", previousGames)
         await powerBall5.delay(10000)
     },
-
-
-//   scrapeData: () => {
-//     tempTime++
-//     try {
-//       const now = moment()
-//       const minutes = now.minutes()
-//       const seconds = now.seconds()
-//       const roundId = powerball5.generateRoundId(interval)
-
-//       if ((minutes + 1) % interval === 0 && seconds === 35) {
-//         const result = {
-//           gameName: game,
-//           date: now.format('YYYY-MM-DD'),
-//           round: roundId,
-//           gameDateTime: now.format('YYYY-MM-DDTHH:mm:ss.SSS') 
-//         };
-
-//         console.log(result)
-//       }
-
-//     } catch (err) {
-//       console.error(`An error occurred - ${game}:`, err)
-//     }
-//   },
-  
-
-//   generateRoundId: (minute) => {
-//     var now = new Date()
-//     var referenceTime = new Date(now)
-//     referenceTime.setHours(0,0,0,0)
-//     var intervalsElapsed = Math.floor((now - referenceTime) / (minute * 60 * 1000))
-//     return intervalsElapsed + 1
-//   },
-//   getAheadTimeBaseOnInterval: (currentMin) => {
-//     let timeIncrement = currentMin
-//     while (true) {
-//         timeIncrement++
-//         if (timeIncrement % interval === 0) {
-//             return timeIncrement
-//         }
-//     }
-// }
-// }
-// function generateRoundId(minute) {
-//   var now = new Date()
-
-//   var referenceTime = new Date(now)
-//   referenceTime.setHours(0,0,0,0)
-
-//   var intervalsElapsed = Math.floor((now - referenceTime) / (minute * 60 * 1000))
-
-//   var roundId = intervalsElapsed + 1
-
-//   return roundId
 }
-// setInterval(() => {
-//   console.log(generateRoundId(5))
-// }, 100000)
 
 module.exports = powerBall5
 
